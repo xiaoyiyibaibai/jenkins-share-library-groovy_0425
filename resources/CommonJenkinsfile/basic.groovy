d460ae425f55d0c686c155e36d6dc7ede1b0d9cd
xiaodonghong <a250604@sina.com>
2019-04-25 11:13:18 +0800

    添加可运行的代码。。

--- resources/CommonJenkinsfile/basic.groovy
+++ resources/CommonJenkinsfile/basic.groovy
@@ -1,1 +1,42 @@
-node
+timestamps{
+    try {
+        node(){
+            writeFile(
+                    file: 'common.groovy',
+                    encoding: 'UTF-8',
+                    text: libraryResource("CommonJenkinsfile/common/common.groovy")
+            );
+          def common = load("common.groovy");
+           println(common.xiao);
+            common.showEcho()
+
+            stage('prepare'){
+                println('--------- prepare-------')
+            }
+            stage('before'){
+                println('--------- before-------')
+            }
+            def hong = '我是start stage 之前的内容'
+            stage('start'){
+                println('--------- start-------'+hong)
+            }
+
+            stage('end'){
+                println('--------- end-------')
+            }
+            def dong = '我是ater stage 之前的内容'
+            stage('after'){
+                println('--------- after -------+dong')
+            }
+        }
+    }
+    catch (e){
+        println(e)
+        mail(
+                subject:"出现了异常！",
+                to: "2367808698@qq.com",
+                from:"a250604@sina.com",
+                body : env.BUILD_URL
+        )
+    }
+}
---
+++ resources/CommonJenkinsfile/common/common.groovy
@@ -0,0 +1,7 @@
+def xiao = "我是肖东红";
+
+def showEcho(){
+    echo 'showEcho 方法'
+    println("println showEcho");
+}
+return this;
